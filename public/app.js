352b434e7eda89302c66b2337a493736cf540553
kani <kanimozhichandrasekar05@gmail.com>
2024-12-02 14:34:56 +0000

    changed a syntax

---
+++ public/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const vitalsForm = document.getElementById('vitalsForm');
+    const vitalsTableBody = document.querySelector('#vitalsTable tbody');
+});
