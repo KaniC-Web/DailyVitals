9ae893873db5d9c37c012ed90fbcbcd8f92488df
kani <kanimozhichandrasekar05@gmail.com>
2024-12-06 09:24:16 +0000

    updated for Mongo

--- app.js
+++ app.js
@@ -27,14 +27,16 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Daily Vitals API!');
 });
 
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
 // Global Error Handler (handles all uncaught errors)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.status || 500).json({ error: err.message || 'Something went wrong!' });
 });
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
