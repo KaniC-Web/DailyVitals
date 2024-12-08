57c9c46131515497a8a64171d71390eb1a61f232
kani <kanimozhichandrasekar05@gmail.com>
2024-12-08 22:52:53 +0000

    port update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5001;
+app.listen(5001, () => {
+  console.log(`Server running on http://localhost:${5001}`);
 });
