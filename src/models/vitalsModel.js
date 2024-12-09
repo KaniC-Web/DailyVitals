0d83a53535dc4eda2f53d4bc87ab29c6688d7e8b
kani <kanimozhichandrasekar05@gmail.com>
2024-12-09 00:45:16 +0000

    updated the code

--- src/models/vitalsModel.js
+++ src/models/vitalsModel.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
 
 const vitalSchema = new mongoose.Schema({
-  heartRate: {
-    type: Number,
-    required: true
-  },
-  bloodPressure: {
-    type: Number,
-    required: true
-  },
-  temperature: {
-    type: Number,
-    required: true
-  }
+  heartRate: { type: Number, required: true },
+  bloodPressure: { type: String, required: true },
+  temperature: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('Vital', vitalSchema);
+const Vital = mongoose.model('Vital', vitalSchema);
+
+module.exports = Vital;
