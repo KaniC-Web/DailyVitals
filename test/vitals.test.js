9430fe55a9607c846e58042511da4252eb7dbe94
kani <kanimozhichandrasekar05@gmail.com>
2024-12-11 17:14:25 +0000

    test for creating vital

--- test/vitals.test.js
+++ test/vitals.test.js
@@ -1,2 +1,13 @@
 const request = require('supertest');
 const app = require('../server'); // Import your server file
+describe('Vitals API', () => {
+    // Test for creating a vital
+    it('should create a new vital', async () => {
+      const newVital = {
+        heartRate: 72,
+        bloodPressure: 120,
+        temperature: 98.6,
+      };
+      const response = await request(app)
+      .post('/api/vitals') // API endpoint for creating a vital
+      .send(newVital); // Send data as request body
